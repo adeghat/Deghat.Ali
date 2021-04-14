ecaaf874887e9269a5d31460a83e86518cbf1d68
ali deghat <deghat.ali@gmail.com>
2021-04-14 21:56:21 +0430

    Add project files.

---
+++ Deghat.Ali/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Deghat.Ali.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
